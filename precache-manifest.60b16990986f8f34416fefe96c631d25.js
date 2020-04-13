a4ed241c8120f36a70bb76b5044cac141c086482
jagmeet singh <jagmeet.alagh@gmail.com>
2020-04-14 03:26:52 +0530

    Updates

---
+++ precache-manifest.60b16990986f8f34416fefe96c631d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd42ad2f0b359a6e094d71c05f67185",
+    "url": "/github-viewer-react/index.html"
+  },
+  {
+    "revision": "57f1608fe0f497353e6e",
+    "url": "/github-viewer-react/static/css/main.6cd76b8a.chunk.css"
+  },
+  {
+    "revision": "4df32d757dc1457e24e4",
+    "url": "/github-viewer-react/static/js/2.15566a61.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/github-viewer-react/static/js/2.15566a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f1608fe0f497353e6e",
+    "url": "/github-viewer-react/static/js/main.34bc7cbb.chunk.js"
+  },
+  {
+    "revision": "dd426ccad7de9ee247a8",
+    "url": "/github-viewer-react/static/js/runtime-main.197e93ec.js"
+  }
+]);
